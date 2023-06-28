26164650ca0f9552a6d773d19224e45e42c3e576
Matteo <mcombi@redhat.com>
2023-06-28 13:50:58 +0200

    Ui improvements

--- order-store-fe/src/main/webapp/src/components/List.js
+++ order-store-fe/src/main/webapp/src/components/List.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 const List = (props) => {
   const { repos } = props;
   if (!repos || repos.length === 0) return <p>No orders, sorry</p>;
@@ -9,7 +10,9 @@ const List = (props) => {
         return (
           <li key={repo.id} className='list'>
             <span className='repo-text'>{repo.name} </span>
-            <span className='repo-description'>{repo.description}</span>
+             <span className='repo-text'>Item Description: </span><span className='repo-description'> {repo.description}</span>
+             <span className='repo-text'>Quantity:  </span><span className='repo-description'>{repo.quantity}</span>
+             <span className='repo-text'>Category: </span><span className='repo-description'>{repo.itemCategory}</span>
           </li>
         );
       })}
