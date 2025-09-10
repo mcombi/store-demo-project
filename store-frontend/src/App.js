c90195325712a37cd46298ca632678321f9dc4e5
Matte C <mcombi@redhat.com>
2025-09-10 15:08:29 +0200

    Created fe app

---
+++ store-frontend/src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import List from './components/List';
+import withListLoading from './components/withListLoading';
+import axios from 'axios'
+import { Config } from './config';
+
+
+function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = 'http://store-route-store-demo-test.apps.cluster-xsn7c.dynamic.redhatworkshops.io/order';
+    axios.get(apiUrl).then((repos) => {
+      const allRepos = repos.data;
+      setAppState({ loading: false, repos: allRepos });
+    });
+  }, [setAppState]);
+
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>Quarkus Shop</h1>
+
+        <img className='quarkusimg' src='img.png'/>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <footer>
+        <div className='footer'>
+
+
+        </div>
+      </footer>
+    </div>
+  );
+}
+export default App;
